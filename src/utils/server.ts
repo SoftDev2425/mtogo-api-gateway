94db0381688c5a4f803852a08cee9d7fc6ac1bdd
Owais Dashti <owaisdashti@outlook.com>
2024-12-04 17:58:03 +0100

    fix: add Swagger UI integration and YAML support

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -5,6 +5,10 @@ import { logRequestDetails } from '../middleware/loggerMiddleware';
 import cookieParser from 'cookie-parser';
 import { jsonParseErrorHandler } from '../middleware/jsonParseErrorHandler';
 import cors from 'cors';
+import * as fs from 'fs';
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path';
 
 export const MAX_SESSIONS = 5;
 
@@ -20,6 +24,17 @@ function createServer() {
   app.use(express.json());
   app.use(jsonParseErrorHandler);
   app.use(cookieParser());
+
+  const swaggerFilePath = path.resolve(__dirname, '../swagger/swagger.yaml');
+
+  if (!fs.existsSync(swaggerFilePath)) {
+    throw new Error(`Swagger file not found at ${swaggerFilePath}`);
+  }
+
+  const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
+  const swaggerDocument = YAML.parse(swaggerFile);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(limiter);
 
